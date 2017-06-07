834b5b1888bcbdb2fa86c1870fe6a2d93c4b13ef
T-LAPTOP\count_t <fangcheng.jia@gmail.com>
2017-06-07 22:48:09 +1200

    update server

--- azuredjango/settings.py
+++ azuredjango/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'azuredjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'heartytravel',
+        'USER': 'postgres',
+        'PASSWORD': '100First!',
+        'HOST': 'heartytravel-postgresqldbserver',
+        'PORT': '5432',
     }
 }
 
